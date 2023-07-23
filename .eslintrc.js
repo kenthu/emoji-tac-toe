21ddeb5464da31232f91f1d34990c460fd545219
Kent Hu <kenthu@kenthu.net>
2023-07-23 14:19:41 -0700

    Add Prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,18 @@
 module.exports = {
   root: true,
-  parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint"],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
   extends: [
-    "next/core-web-vitals",
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:import/recommended",
-    "plugin:import/typescript",
-    "plugin:react-hooks/recommended",
+    'next/core-web-vitals',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'plugin:react-hooks/recommended',
+    'prettier',
   ],
   rules: {
-    "import/order": [
-      "error",
-      { "newlines-between": "always", alphabetize: { order: "asc" } },
-    ],
-    "react-hooks/exhaustive-deps": "error",
+    'import/order': ['error', { 'newlines-between': 'always', alphabetize: { order: 'asc' } }],
+    'react-hooks/exhaustive-deps': 'error',
   },
 };
